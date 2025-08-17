06a460b6e9de09552e8dec36212c0cfd678ddfa1
Dev Dimma <elizabethndubuisi8@gmail.com>
2025-08-17 16:02:27 +0100

    added API integration

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
+  let quoteSpaceElement = document.querySelector("#quote-area");
+  quoteSpaceElement.style.display = "block";
   new Typewriter('#quote-area', {
-  strings: "hello",
+  strings: response.data.answer,
   autoStart: true,
-  delay : 1,
+  delay : 10,
   cursor: null
 });
+}
+
+
+function generateQuote(event) {
+event.preventDefault();
+
+let userInstructions = document.querySelector("#user-instructions");
+let apiKey = "a6469t9f026f33001d9f3f695ocb3042";
+let prompt = `Generate a neutral quote based on ${userInstructions.value}`;
+let context = "You are a quote AI assistant that generates quote. The quote should be neutral depending on users input, reflecting the essence of the user's quote, please print it out in basic html format and only h1 and p tags should be used, use emojis too";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayQuote);
 }
 
 
