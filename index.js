cbb4a987bce36c8614d9b68c459dda7cbda7b27c
Dev Dimma <elizabethndubuisi8@gmail.com>
2025-08-17 17:04:38 +0100

    Final touches

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function displayQuote(response) {
-  let quoteSpaceElement = document.querySelector("#quote-area");
-  quoteSpaceElement.style.display = "block";
   new Typewriter('#quote-area', {
   strings: response.data.answer,
   autoStart: true,
@@ -18,6 +16,10 @@ let apiKey = "a6469t9f026f33001d9f3f695ocb3042";
 let prompt = `Generate a neutral quote based on ${userInstructions.value}`;
 let context = "You are a quote AI assistant that generates quote. The quote should be neutral depending on users input, reflecting the essence of the user's quote, please print it out in basic html format and only h1 and p tags should be used, use emojis too";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let quoteSpaceElement = document.querySelector("#quote-area");
+  quoteSpaceElement.classList.remove("hide");
+  quoteSpaceElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${userInstructions.value}...</div>`;
 
 axios.get(apiUrl).then(displayQuote);
 }
